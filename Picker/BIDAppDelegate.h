2778fccce32e1e1733ba2b7435bb2cbd5050472d
Daniel Moore <danmr4@gmail.com>
2013-08-10 09:34:44 -0400

    Initial Commit

---
+++ Picker/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Picker
+//
+//  Created by daniel moore on 8/10/13.
+//  Copyright (c) 2013 daniel moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
