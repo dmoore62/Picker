b89d40f3f1f1e09adbf966fc3563331243b3f6e2
Daniel Moore <danmr4@gmail.com>
2013-08-13 17:18:15 -0400

    page 194 with dateFormatter

--- Picker/BIDDatePickerViewController.m
+++ Picker/BIDDatePickerViewController.m
@@ -27,12 +27,29 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    NSDate *now = [NSDate date];
+    [self.datePicker setDate:now animated:NO];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)buttonpressed
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+    [dateFormatter setTimeStyle:NSDateFormatterMediumStyle];
+    
+    NSDate *selected = [self.datePicker date];
+    NSString *formatted_time = [dateFormatter stringFromDate:selected];
+    NSString *message = [[NSString alloc] initWithFormat:@"The date and time you selected is: %@", formatted_time];
+    
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle:@"Date and Time Selected" message:message delegate:nil cancelButtonTitle:@"Done" otherButtonTitles:nil];
+    [alert show];
 }
 
 @end
