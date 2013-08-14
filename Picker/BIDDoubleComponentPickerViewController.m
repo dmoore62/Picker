7e35fb3852ccb0094487c19d7e1569ec44fa06b1
Daniel Moore <danmr4@gmail.com>
2013-08-14 17:34:11 -0400

    adds single and double pickers to tabs

--- Picker/BIDDoubleComponentPickerViewController.m
+++ Picker/BIDDoubleComponentPickerViewController.m
@@ -27,12 +27,55 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.fillingTypes = @[@"Turkey", @"Ham", @"Peanut Butter", @"Manwich"];
+    self.breadTypes= @[@"White", @"Wheat", @"Sourdough", @"Pita"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)buttonPressed
+{
+    NSInteger fillingRow = [self.doublePicker selectedRowInComponent:kFillingComponent];
+    NSInteger breadRow = [self.doublePicker selectedRowInComponent:kBreadComponent];
+    
+    NSString *filling = self.fillingTypes[fillingRow];
+    NSString *bread = self.breadTypes[breadRow];
+    
+    NSString *message = [[NSString alloc] initWithFormat:@"You're %@ on %@ will be right up!", filling, bread];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Thank you for your order" message:message delegate:nil cancelButtonTitle:@"Great" otherButtonTitles: nil];
+    
+    [alert show];
+}
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 2;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    if(component == kBreadComponent){
+        return [self.breadTypes count];
+    }else{
+        return [self.fillingTypes count];
+    }
+}
+
+#pragma mark Picker Delegate Methods
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if(component == kBreadComponent){
+        return self.breadTypes[row];
+    }else{
+        return self.fillingTypes[row];
+    }
 }
 
 @end
